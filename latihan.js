c6fdfd8eb1e7a0e502cba503e8cdc3c803fb11f5
f.hanifah.hub <f.hanifah.ofc@gmail.com>
2020-12-09 14:19:51 +0700

    pancake

---
+++ latihan.js
@@ -0,0 +1,19 @@
+//diferent variables sould have their own "let"
+//let var1 = 'baju';
+//let var2 = 'celana';
+
+// --many variables with one "let"
+// let variables;
+// variables = 'candle';
+// variables = 'kind a like'
+
+//datatypes
+//const sweet = 17;
+//console.log(variables);
+// operators
+//console.log(9 + 21, 'pancake' + 'blueberry', 12 / 3, 2 * 2 === 2 + 2, 4 === 12 / 3, );
+
+//with variables
+const cool = 16;
+const swag = 7;
+console.log(cool + swag);
